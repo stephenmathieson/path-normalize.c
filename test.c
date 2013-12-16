ed63c4d9428f9dd6b9553bb06c9ba5c6c4c8acdb
Stephen Mathieson <smath23@gmail.com>
2013-12-15 19:10:46 -0500

    Don’t rely on strdup

--- test.c
+++ test.c
@@ -1,17 +1,12 @@
 
-#include <stdio.h>
-
 #include <string.h>
 #include <assert.h>
 #include "path-normalize.h"
 
 int main(int argc, char **argv) {
-
   assert(NULL == path_normalize(NULL));
   assert(0 == strcmp("/home/stephen/", path_normalize("/home//stephen//")));
   assert(0 == strcmp("home/stephen/", path_normalize("home//stephen//")));
   assert(0 == strcmp("/home/stephen/", path_normalize("//home/stephen//")));
-
-
   return 0;
 }
